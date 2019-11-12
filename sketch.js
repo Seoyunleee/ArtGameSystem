2302befcd5e775c39eab2377b37b56e9921d297c
SeoYunLee <55418036+Seoyunleee@users.noreply.github.com>
2019-11-12 11:27:36 +0900

    sprite - creation

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
-
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  createCanvas(800, 400);
 }
 
 function draw() {
-  background(30);
-  rect(mouseX, mouseY, 100, 100);
+  background(255, 255, 255);
+
+  fill(0);
+  textAlign(CENTER);
+  text('Click to create a new sprite', width/2, height/2);
+  drawSprites();
+}
+
+function mousePressed() {
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
 }
